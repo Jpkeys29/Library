c0c50603e8fe3cb238ba427680b302a4e4638ae0
Jpkeys29 <jpmusik29@gmail.com>
2023-02-12 16:07:29 -0500

    delete view

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('create_book/',views.BookCreate.as_view(),name='create_book'),
     path('book/<int:pk>/',views.BookDetail.as_view(),name='book_detail'),
-    path('book_list/', views.BookList.as_view(), name='book_list')
+    path('book_list/', views.BookList.as_view(), name='book_list'),
+    path('book_delete/<int:pk>',views.BookDelete.as_view(), name='book_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 #To return a simple "It's working" import HttpResponse
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from .models import Book,Author,BookInstance,Genre,Language
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, DeleteView
 
 
 # Create your views here.
@@ -26,15 +27,26 @@ def index(request):
 class BookCreate(CreateView): #model_form.html
     model = Book #connects to the Book model
     fields = '__all__'
+    #If you want to pass specific fields, create a list:
+    #fields = ['first_name','last_name']
 
 class BookDetail(DetailView):
     model = Book
 
 
-
+#Read 
 class BookList(ListView):
     model = Book
     field = '__all__'
     
+#Delete
+class BookDelete(DeleteView):
+    #default template name: model_confirm-delete.html
+    #It is a form that goes to confirm Delete Button
+    model = Book
+    #success_url is also an attribute and represents the URL you want to go to after the form has been submitted
+    # success_url is NOT a template. It is the actual URL
+    success_url = reverse_lazy('book_list')
+
 
 
